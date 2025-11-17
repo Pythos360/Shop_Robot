aeca7ba7994a25c8c2a7bf457c8029ee2345e759
Benjamin Erickson <benjamin.erickson2003@gmail.com>
2025-11-17 14:22:20 -0700

    added control to path

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
             'controller = my_pi_nodes.controller:main',
             'serial_bridge = my_pi_nodes.serial_bridge:main',
             'MotorCmd = my_pi_nodes.MotorCmd:main',
+            'Control = my_pi_nodes.Control:main',
         ],
     },
 )
