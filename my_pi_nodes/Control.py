83ece1fe0c67cd6ef6b3b5fdc7659b62a8d3bc5c
Benjamin Erickson <benjamin.erickson2003@gmail.com>
2025-11-19 12:47:54 -0700

    slight change

--- my_pi_nodes/Control.py
+++ my_pi_nodes/Control.py
@@ -258,7 +258,7 @@ class dynamics:
 
         return self.J
     
-    def qdot_from_v(self, v, gain=20):
+    def qdot_from_v(self, v, gain=200):
         # Get joystick axes (assume first three are x,y,z directions)
         
 
